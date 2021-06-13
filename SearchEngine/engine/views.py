8165b0f3349c02cd7698085ffc5f6a11c07d6f80
Harshita Nag <42053832+sudoharshita16@users.noreply.github.com>
2021-06-13 18:39:28 +0530

    Added pagerank function

--- SearchEngine/engine/views.py
+++ SearchEngine/engine/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup as bsf
-import pickle
 
 import html2text
 from nltk.tokenize import sent_tokenize, word_tokenize
@@ -9,6 +8,17 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk import RegexpTokenizer
 from collections import Counter
+
+import pickle
+import os
+import random
+import re
+import sys
+import math
+
+DAMPING = 0.85
+SAMPLES = 10000
+
 
 # Create your views here.
 # link_site = 'https://xkcd.com/2173/'
@@ -40,6 +50,7 @@ def get_crawled():
         pickle_out = open("crawled.pickle","wb")
         pickle.dump(crawled, pickle_out)
         pickle_out.close()
+        iterate_pagerank(crawled, DAMPING)
         return crawled
 
 
@@ -82,3 +93,64 @@ def filter_page(page):
     return FW
     #returns a dictionary with word frequency
 
+
+def iterate_pagerank(corpus, damping_factor):
+    """
+    Returns PageRank values for each page by iteratively updating
+    PageRank values until convergence.
+    Returns a dictionary where keys are page names, and values are
+    their estimated PageRank value (a value between 0 and 1).
+    """
+    for page in corpus:
+        if len(corpus[page]) == 0:
+            corpus.pop(page)
+            #corpus[page] = list(corpus[page])
+            #for p in corpus:
+                #corpus[page].append(p)
+         
+    revcorpus = rev_corpus(corpus)
+    #print("REVERSE")
+    #print(revcorpus)
+    pagerank_dict = {}
+    no_of_pages = len(revcorpus)
+    for key in revcorpus:
+        pagerank_dict[key] = 1/no_of_pages
+    print(pagerank_dict)    
+    counter = 0
+    while(counter < no_of_pages):
+        counter = 0
+        for key in revcorpus:
+            new_rank = 0
+            revcorpus[key] = list(revcorpus[key])
+            for value in revcorpus[key]:
+                new_rank =  new_rank + (pagerank_dict[value]/len(corpus[value]))    
+            new_rank =  (1-damping_factor)/no_of_pages + damping_factor*new_rank
+        
+            if abs(new_rank - pagerank_dict[key]) <= 0.001:
+                counter +=1 
+            else:
+                pagerank_dict[key] = new_rank                
+    return pagerank_dict       
+
+def rev_corpus(corpus):
+    dic = {}
+    for key in corpus:
+        dic[key] = []
+    for key in corpus:
+        corpus[key] = list(corpus[key])
+        for val in corpus[key]:
+            if val not in dic:
+                dic[val]=[]
+            dic[val].append(key)
+
+    return (dic)     
+
+
+def rank(request):
+    corpus = get_crawled()
+    #print(corpus)
+    pagerank = iterate_pagerank(corpus, DAMPING)
+    #print(pagerank)
+    return render(request,'engine/rank.html',{
+        "pagerank":pagerank
+    })    
