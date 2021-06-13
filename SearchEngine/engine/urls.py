3960682c53a64932446eee91bff755528e4eef00
Harshita Nag <42053832+sudoharshita16@users.noreply.github.com>
2021-06-13 18:41:15 +0530

    Updated urls.py
    Added "/rank" route

--- SearchEngine/engine/urls.py
+++ SearchEngine/engine/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("crawl",views.crawl, name="crawl")
+    path("crawl",views.crawl, name="crawl"),
+    path("rank",views.rank,name="rank"),
 ]
