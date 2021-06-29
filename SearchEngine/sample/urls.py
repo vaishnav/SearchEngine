ec4d5d6d17ea0e34a0d07294ec6a9f4b24a0a2a1
Vaishnav Sharma <30750862+vaishnav@users.noreply.github.com>
2021-06-30 00:36:37 +0530

    add sample pages

--- SearchEngine/sample/urls.py
+++ SearchEngine/sample/urls.py
@@ -4,4 +4,13 @@ from . import views
 
 urlpatterns = [
     path("one",views.one,name="one"),
+    path("two",views.two,name="two"),
+    path("three",views.three,name="three"),
+    path("four",views.four,name="four"),
+    path("five",views.five,name="five"),
+    path("six",views.six,name="six"),
+    path("seven",views.seven,name="seven"),
+    path("eight",views.eight,name="eight"),
+    path("nine",views.nine,name="nine"),
+    path("ten",views.ten,name="ten"),
 ]
--- SearchEngine/sample/views.py
+++ SearchEngine/sample/views.py
@@ -5,3 +5,30 @@ from django.shortcuts import render
 
 def one(request):
     return render(request,'sample/one.html')   
+
+def two(request):
+    return render(request,'sample/two.html')   
+
+def three(request):
+    return render(request,'sample/three.html')   
+
+def four(request):
+    return render(request,'sample/four.html')   
+
+def five(request):
+    return render(request,'sample/five.html')   
+
+def six(request):
+    return render(request,'sample/six.html')   
+
+def seven(request):
+    return render(request,'sample/seven.html')   
+
+def eight(request):
+    return render(request,'sample/eight.html')   
+
+def nine(request):
+    return render(request,'sample/nine.html')  
+
+def ten(request):
+    return render(request,'sample/ten.html')   
